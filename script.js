6de3aea37d51a13ad706d300fe0155649497a475
Ravi Kumar Patel <109025108+ravikrpatel01@users.noreply.github.com>
2024-06-26 23:46:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 function getInputValue() {
-    let heightInCM = document.getElementById('height').value;
+    let resultPrint = document.getElementById('result');
+    let resultString = document.getElementById('p1');
 
+    resultString.style.display = 'block';
+
+    let heightInCM = document.getElementById('height').value;
     let weightInKG = document.getElementById('weight').value;
 
-    console.log(heightInCM);
-    console.log(weightInKG);
+    if (String(heightInCM) === "" || String(weightInKG) === "") {
+        resultPrint.style.color = 'red';
+        resultPrint.style.fontWeight = 600;
+        resultPrint.style.fontSize = '18px';
+        resultPrint.innerHTML = 'Please ENTER your Height and Weight!';
+        resultString.style.display = 'none';
+    }else{
+        let heightInMeter = heightInCM / 100;
 
-    let heightInMeter = heightInCM / 100
+        let bmi = weightInKG / (heightInMeter * heightInMeter);
+    
+        console.log(bmi.toFixed(2));
 
-    let bmi = weightInKG / (heightInMeter * heightInMeter);
-
-    console.log(bmi.toFixed(2));
-
-    let resultPrint = document.getElementById('result');
-
-    resultPrint.innerHTML = `Your BMI is ${bmi.toFixed(2)}.`;
+        resultPrint.style.color = 'black';
+        resultPrint.style.fontWeight = 100;
+        resultPrint.style.fontSize = '17px';
+        
+        resultPrint.innerHTML = `Your BMI is ${bmi.toFixed(2)}.`;
+    }
 }
 
